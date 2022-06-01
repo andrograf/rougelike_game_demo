666cbc43f9f158c6dd054b6d241af5e728764098
andrograf <patriciapusztai@gmail.com>
2022-06-01 13:01:11 +0200

    Add demage, corpses

--- game_map.py
+++ game_map.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from typing import Iterable, Optional, TYPE_CHECKING
+from typing import Iterable, Iterator, Optional, TYPE_CHECKING
 import numpy as np  # type: ignore
 from tcod.console import Console
-
+from entity import Actor
 import tile_types
 if TYPE_CHECKING:
     from engine import Engine
@@ -20,10 +20,26 @@ class GameMap:
         self.visible = np.full((width, height), fill_value=False, order="F")  # Tiles the player can currently see
         self.explored = np.full((width, height), fill_value=False, order="F")  # Tiles the player has seen before
 
+    @property
+    def actors(self) -> Iterator[Actor]:
+        """Iterate over this maps living actors."""
+        yield from (
+            entity
+            for entity in self.entities
+            if isinstance(entity, Actor) and entity.is_alive
+        )
+
     def get_blocking_entity_at_location(self, location_x: int, location_y: int,) -> Optional[Entity]:
         for entity in self.entities:
             if (entity.blocks_movement and entity.x == location_x and entity.y == location_y):
                 return entity
+
+        return None
+
+    def get_actor_at_location(self, x: int, y: int) -> Optional[Actor]:
+        for actor in self.actors:
+            if actor.x == x and actor.y == y:
+                return actor
 
         return None
 
